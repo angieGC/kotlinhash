6e25617972d6d32e462dd03bbb9cf489b4f1bfb5
angieGC <angiecata.30@gmail.com>
2022-07-11 14:44:22 -0500

    file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,10 +9,11 @@ fun main(args: Array<String>) {
 
     var arraylistHs = ArrayList<ArrayList<String>>()
     var arraylistN = ArrayList<String>()
-    File("D:/Angie/universidad/2020/imagesdataset/VG_100K/").walk().forEach { it ->
-    //File("D:/Angie/trabajoimagenes/org-img/impresas/").walk().forEach { it ->
+    //File("D:/Angie/trabajoimagenes/in-img/tensor/").walk().forEach { it ->
+    //File("D:/Angie/universidad/2020/imagesdataset/VG_100K/").walk().forEach { it ->
+    File("D:/Angie/trabajoimagenes/org-img/impresas/").walk().forEach { it ->
         //println(it.extension + " is the extension of " + it.name)
-        if (it.extension == "jpg")
+        if (it.extension == "png")
         {
             var arraylistB = ArrayList<String>()
             var arraylistH = ArrayList<String>()
@@ -27,7 +28,7 @@ fun main(args: Array<String>) {
             hash.forEach {
 
                 val hexString = String.format("%X", it)
-                // println(hexString)
+                //print(hexString)
                 if (h == null)
                 {
                     h = hexString
@@ -35,7 +36,15 @@ fun main(args: Array<String>) {
                     h += hexString
                 }
             }
+
             var ha = h.toString()
+            while(ha.length < 16)
+            {
+                //println(ha.length)
+                ha = "0$ha"
+            }
+            //println(ha)
+
             ha.forEach {
                 val decimalString = BigInteger(it.toString(), 16).toString()
                 arraylistH.add(decimalString)
@@ -46,8 +55,8 @@ fun main(args: Array<String>) {
 
     }
 
-    File("src/main/resources/file2.txt").writeText(arraylistHs.joinToString())
-    File("src/main/resources/file22.txt").writeText(arraylistN.joinToString())
+    File("src/main/resources/file1.txt").writeText(arraylistHs.joinToString())
+    File("src/main/resources/file11.txt").writeText(arraylistN.joinToString())
     val filename = "file2.txt"
 
     var fileObject = File(filename)
